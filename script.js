b8ecabe9f8cf79da22a224986f684b6c2ce84cea
burakbozduman-code <burakbzdn@icloud.com>
2026-07-23 18:01:37 +0300

    Implement countdown timer and button display logic
    Added countdown timer functionality and button interaction.

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("Edanur & Müslüm Burak");
+const buton = document.getElementById("ac");
+const davet = document.getElementById("davet");
+const loading = document.querySelector(".loading");
+
+buton.onclick = function () {
+    loading.style.display = "none";
+    davet.style.display = "block";
+}
+
+const hedef = new Date("2026-08-30T20:00:00").getTime();
+
+function geriSayim() {
+
+    const simdi = new Date().getTime();
+
+    const fark = hedef - simdi;
+
+    const gun = Math.floor(fark / (1000 * 60 * 60 * 24));
+
+    const saat = Math.floor((fark % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    const dakika = Math.floor((fark % (1000 * 60 * 60)) / (1000 * 60));
+
+    const saniye = Math.floor((fark % (1000 * 60)) / 1000);
+
+    document.getElementById("sayac").innerHTML =
+        gun + " Gün " +
+        saat + " Saat " +
+        dakika + " Dakika " +
+        saniye + " Saniye";
+}
+
+setInterval(geriSayim,1000);
+geriSayim();
